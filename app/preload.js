582373acc4fcdb51cca02d9a4f50ef2a72510248
shutaozhenzhen <3462232313@qq.com>
2021-06-30 21:26:23 +0800

    读取文件

--- app/preload.js
+++ app/preload.js
@@ -1,4 +1,7 @@
 const marked = require('marked')
+const fs = require('fs')
+const iconv = require('iconv-lite')
+const chardet = require('chardet')
 const {
 	contextBridge,
 	ipcRenderer,
@@ -11,11 +14,24 @@ contextBridge.exposeInMainWorld(
 			return marked(markdown)
 		},
 		'openDialog': () => {
-			ipcRenderer.invoke('openDialog',{
+			ipcRenderer.invoke('openDialog', {
 				'properties': ['openFile']
 			}).then((result) => {
-				console.log(result)
+				if (!result.canceled) {
+					let path=result.filePaths[0]
+					chardet.detectFile(path, {
+							sampleSize: 1024
+						})
+						.then(encoding => {
+							fs.createReadStream(path)
+							.pipe(iconv.decodeStream(encoding))
+							.on('data',(d)=>console.log(d))
+						})
+				}
 			})
+		},
+		'openFile': (path) => {
+
 		}
 	}
 )
